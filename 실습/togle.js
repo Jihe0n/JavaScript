567b5b4782da1f715975fa1bb947b41fa6b04008
heon <siwmua0@naver.com>
2022-02-13 14:39:44 +0900

    flex toogle navbar 연습

---
+++ 실습/togle.js
@@ -0,0 +1,10 @@
+
+
+const btn = document.querySelector(".btn")
+const nav = document.querySelector(".nav")
+const icon = document.querySelector(".icon")
+
+btn.addEventListener("click", ()=> {
+    nav.classList.toggle("state")
+    icon.classList.toggle("state")
+})
