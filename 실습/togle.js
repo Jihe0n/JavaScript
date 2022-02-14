951d6abcd2d0c2c2e54ff377c4cdce67fa37512a
heon <siwmua0@naver.com>
2022-02-14 10:00:08 +0900

    youtube

--- 실습/togle.js
+++ 실습/togle.js
@@ -1,10 +1,8 @@
-
-
 const btn = document.querySelector(".btn")
 const nav = document.querySelector(".nav")
 const icon = document.querySelector(".icon")
 
-btn.addEventListener("click", ()=> {
+btn.addEventListener("click", ()=>{
     nav.classList.toggle("state")
     icon.classList.toggle("state")
 })
